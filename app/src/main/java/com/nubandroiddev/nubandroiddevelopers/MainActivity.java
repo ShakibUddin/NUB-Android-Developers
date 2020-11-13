6c04cbcf78c5ec363e37fb3210243b4b2343e052
Md Shakib Uddin Bhuiyan <shakibuddinbhuiyan@gmail.com>
2020-11-13 23:11:40 +0600

    working on sakib

--- app/src/main/java/com/nubandroiddev/nubandroiddevelopers/MainActivity.java
+++ app/src/main/java/com/nubandroiddev/nubandroiddevelopers/MainActivity.java
@@ -1,15 +1,19 @@
-package com.nubandroiddev.nubandroiddevelopers;
+  package com.nubandroiddev.nubandroiddevelopers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.nubandroiddev.nubandroiddevelopers.shakib.ShakibUddin;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -29,11 +33,26 @@ public class MainActivity extends AppCompatActivity {
     //I used TreeMap as it automatically sorts elements
     private TreeMap<String,Integer> data;
     private ArrayList<Profile> profiles;
+    CarouselView carouselView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //list of carosoul images
+        int[] sampleImages = {R.drawable.one, R.drawable.two, R.drawable.three};
+
+        ImageListener imageListener = new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+                imageView.setImageResource(sampleImages[position]);
+            }
+        };
+
+        carouselView = findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
 
         //remove these 2 lines to show the use of logcat
         data = new TreeMap<>();
@@ -48,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
         data.put("Shohan",R.drawable.shohan);
         data.put("Tareq",R.drawable.tareq);
         data.put("Nahid",R.drawable.nahid);
+
+
 
         //filling profiles list with profile object
         for(Map.Entry<String,Integer> entry: data.entrySet()){
@@ -83,6 +104,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     private void openShakibUddin(){
+        Intent intent = new Intent(getApplicationContext(), ShakibUddin.class);
+        startActivity(intent);
+    }
+
+    private void openTareq(){
         Intent intent = new Intent(getApplicationContext(), ShakibUddin.class);
         startActivity(intent);
     }
--- app/src/main/java/com/nubandroiddev/nubandroiddevelopers/shakib/ShakibUddin.java
+++ app/src/main/java/com/nubandroiddev/nubandroiddevelopers/shakib/ShakibUddin.java
@@ -1,16 +1,24 @@
 package com.nubandroiddev.nubandroiddevelopers.shakib;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.nubandroiddev.nubandroiddevelopers.R;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class ShakibUddin extends AppCompatActivity {
+
+    CarouselView carouselView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shakib_uddin);
+
+
     }
 }
